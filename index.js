0b212538672d3bde461afa5f0486c95af2969899
Brittany Young <mrsbrityoung@gmail.com>
2018-01-19 21:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
 function sayHiToGrandma(string) {
   return string.toLowerCase()
 }
